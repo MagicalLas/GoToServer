12f1af0e9434d81e45c2649e1b1482a37d4abd7f
Wonho ha <35015234+Las-Wonho@users.noreply.github.com>
2018-05-29 19:08:07 +0900

    Standard

--- Source/main.go
+++ Source/main.go
@@ -1,5 +1,19 @@
 package main;
 
+import (
+	"net/http"
+)
+
 func main(){
+	arr := [100][100]int{}
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
+        w.Write([]byte("Hello World"))
+    })
+	http.ListenAndServe(":5000", nil)
+	
+	arr[1][2]=3
+	
 	print("Server Start")
+	
 }
